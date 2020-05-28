4ab7c663fe290cdc277c53aee1152964dba6da93
ruanvictorreis <ruanvictorreis@gmail.com>
2020-05-28 00:02:22 -0300

    setting navigation bar

--- TMDB/SupportFiles/AppDelegate.swift
+++ TMDB/SupportFiles/AppDelegate.swift
@@ -10,18 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        if let viewController = MovieListBuilder().build() {
-            let navigation = UINavigationController(rootViewController: viewController)
-            self.window = UIWindow(frame: UIScreen.main.bounds)
-            self.window?.rootViewController = navigation
-            self.window?.makeKeyAndVisible()
-        }
-        
+        // Override point for customization after application launch.        
         return true
     }
 
