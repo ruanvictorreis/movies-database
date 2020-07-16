0d6c151234ebb64f6370812bdf2615adbc8ef3a0
ruanvictorreis <ruanvictorreis@gmail.com>
2020-07-16 19:23:38 -0300

    Fixing broken tests

--- TMDBTests/TMDBTests.swift
+++ TMDBTests/TMDBTests.swift
@@ -22,7 +22,7 @@ class TMDBTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual("Hello world", "Olá mundo")
+        XCTAssertEqual("Hello world", "Hello world")
     }
 
     func testPerformanceExample() throws {
